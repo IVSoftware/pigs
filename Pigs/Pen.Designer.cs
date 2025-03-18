47f9e84acde70df6d0649364e347c13679246653
Tom Gregor <tgregor@ivsoftware.com>
2025-03-18 07:51:15 -0600

    Add project files.

---
+++ Pigs/Pen.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Pigs
+{
+    partial class Pen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            horizontalLayoutPanel = new TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // horizontalLayoutPanel
+            // 
+            horizontalLayoutPanel.ColumnCount = 4;
+            horizontalLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            horizontalLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            horizontalLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            horizontalLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            horizontalLayoutPanel.Dock = DockStyle.Fill;
+            horizontalLayoutPanel.Location = new Point(0, 0);
+            horizontalLayoutPanel.Name = "horizontalLayoutPanel";
+            horizontalLayoutPanel.RowCount = 1;
+            horizontalLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            horizontalLayoutPanel.Size = new Size(710, 185);
+            horizontalLayoutPanel.TabIndex = 0;
+            // 
+            // Pen
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(horizontalLayoutPanel);
+            Name = "Pen";
+            Size = new Size(710, 185);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel horizontalLayoutPanel;
+    }
+}
---
+++ Pigs/Pig.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Pigs
+{
+    partial class Pig
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel = new TableLayoutPanel();
+            comboBox1 = new ComboBox();
+            textBoxID = new TextBox();
+            tableLayoutPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            tableLayoutPanel.ColumnCount = 1;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel.Controls.Add(comboBox1, 0, 0);
+            tableLayoutPanel.Controls.Add(textBoxID, 0, 1);
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.Location = new Point(0, 0);
+            tableLayoutPanel.Name = "tableLayoutPanel";
+            tableLayoutPanel.RowCount = 4;
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel.Size = new Size(150, 150);
+            tableLayoutPanel.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Male", "Female" });
+            comboBox1.Location = new Point(3, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(144, 33);
+            comboBox1.TabIndex = 0;
+            // 
+            // textBoxID
+            // 
+            textBoxID.Location = new Point(3, 40);
+            textBoxID.Name = "textBoxID";
+            textBoxID.PlaceholderText = "Id";
+            textBoxID.Size = new Size(144, 31);
+            textBoxID.TabIndex = 1;
+            // 
+            // Pig
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel);
+            Name = "Pig";
+            tableLayoutPanel.ResumeLayout(false);
+            tableLayoutPanel.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel;
+        private ComboBox comboBox1;
+        private TextBox textBoxID;
+    }
+}
